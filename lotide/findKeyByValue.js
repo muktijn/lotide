1682108babcd18fd56d0b9a750427700b7d96cda
Mukti Jain <muktijn@gmail.com>
2020-09-17 14:57:31 +0000

    findKeyByValue

---
+++ lotide/findKeyByValue.js
@@ -0,0 +1,30 @@
+// FUNCTION IMPLEMENTATION
+
+const findKeyByValue = function(obj, value) {
+  let i;
+  for (i of Object.keys(obj)) {
+    if (obj[i] === value) {
+      return i;
+    }
+  }
+  return undefined;
+}
+
+const assertEqual = function(actual, expected) {
+if (actual === expected) {
+  console.log(`Assertion Passed: ${actual} === ${expected}`);
+} else {
+  console.log(`Assertion Passed: ${actual} !== ${expected}`);
+}
+};
+
+// TEST CODE
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
